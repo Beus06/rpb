651825dc5d7a82bcf2647d04469f8c711c716dc6
Z-Szabadkai <zoltan.szabadkai@gmail.com>
2021-09-04 16:57:38 +0200

    Expanded the game_session and game_session_character class

---
+++ src/main/java/gameSession/Game_Session.java
@@ -0,0 +1,37 @@
+package gameSession;
+
+import javax.print.attribute.standard.DateTimeAtCompleted;
+
+public class Game_Session {
+
+    private long id;
+    private DateTimeAtCompleted game_end_date;
+
+    public Game_Session() {}
+
+    public Game_Session(DateTimeAtCompleted game_end_date) {
+        this.game_end_date = game_end_date;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public DateTimeAtCompleted getGame_end_date() {
+        return game_end_date;
+    }
+
+    public void setGame_end_date(DateTimeAtCompleted game_end_date) {
+        this.game_end_date = game_end_date;
+    }
+
+    @Override
+    public String toString() {
+        return  id + " - " +
+                game_end_date;
+    }
+}
---
+++ src/main/java/gameSession/Game_Session_Character.java
@@ -0,0 +1,38 @@
+package gameSession;
+
+public class Game_Session_Character {
+
+    private long id;
+    private long chr_lvl;
+    private long chr_xp;
+
+    public Game_Session_Character() {}
+
+    public Game_Session_Character(long chr_lvl, long chr_xp) {
+        this.chr_lvl = chr_lvl;
+        this.chr_xp = chr_xp;
+    }
+
+    public long getChr_lvl() {
+        return chr_lvl;
+    }
+
+    public void setChr_lvl(long chr_lvl) {
+        this.chr_lvl = chr_lvl;
+    }
+
+    public long getChr_xp() {
+        return chr_xp;
+    }
+
+    public void setChr_xp(long chr_xp) {
+        this.chr_xp = chr_xp;
+    }
+
+    @Override
+    public String toString() {
+        return  id + " - " +
+                chr_lvl + " - " +
+                chr_xp;
+    }
+}
